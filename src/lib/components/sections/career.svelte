97b3edcd9e6d7e451cdf1067df1488450148ed93
Michael Holley <michael.philipp.holley@gmail.com>
2023-07-20 20:10:27 +0200

    text adjustment

--- src/lib/components/sections/career.svelte
+++ src/lib/components/sections/career.svelte
@@ -4,12 +4,12 @@
 
 <section id="taetigkeiten" class="bg-mhAlt p-8 md:p-16">
 	<SectionHeader title="Tätigkeiten" class="text-center" />
-	<div class="flex flex-col justify-center text-justify text-lg">
+	<div class="flex flex-col justify-center text-lg text-left">
 		<div class="w-10/12 lg:w-2/5 mx-auto my-6">
 			<div class="pl-5 text-xl">Seit Jan. 2022</div>
 			<h4 class="text-2xl font-bold">Software Developer</h4>
 			<div>AraCom IT Services AG</div>
-			<div>
+			<div class="text-justify">
 				Teambasierte Entwicklung von Individual-Software für Kunden aus unterschiedlichsten Branchen
 				unter Einsatz neuester Technologien.
 			</div>
@@ -18,8 +18,10 @@
 			<div class="pl-5 text-xl">Sept. 2019 - Jan. 2022</div>
 			<h4 class="text-2xl font-bold">Ausbildung zum Fachinformatiker für Anwendungsentwicklung</h4>
 			<div>AraCom IT Services AG</div>
-			<div>Berufsschule abgeschlossen mit 1,0</div>
-			<div>IHK Prüfung abgelegt mit <strong>96 von 100</strong> Punkten gesamt</div>
+			<div class="text-justify">Berufsschule abgeschlossen mit 1,0</div>
+			<div class="text-justify">
+				IHK Prüfung abgelegt mit <strong>96 von 100</strong> Punkten gesamt
+			</div>
 		</div>
 	</div>
 </section>

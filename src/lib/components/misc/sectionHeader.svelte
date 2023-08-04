890e6f1c42eacb2b5db344c58b37091e0f9cedd5
Michael Holley <michael.philipp.holley@gmail.com>
2023-08-04 20:36:50 +0200

    minor text scaling fixes

--- src/lib/components/misc/sectionHeader.svelte
+++ src/lib/components/misc/sectionHeader.svelte
@@ -2,4 +2,4 @@
 	export let title: string;
 </script>
 
-<h1 class="text-7xl font-bold lowercase {$$props.class}">{title}</h1>
+<h1 class="text-5xl sm:text-7xl font-bold lowercase {$$props.class}">{title}</h1>

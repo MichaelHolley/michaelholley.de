16550f2b86a7ac26d6a6047854497b706fd2e140
Michael Holley <michael.philipp.holley@gmail.com>
2023-08-15 22:16:23 +0200

    added shadow to header image

--- src/lib/components/sections/header.svelte
+++ src/lib/components/sections/header.svelte
@@ -43,7 +43,7 @@
 		<img
 			src={MeImg}
 			alt="Michael Holley"
-			class="w-[250px] lg:w-[350px] align-middle mx-auto lg:mx-0 shadow-2xl"
+			class="w-[250px] lg:w-[350px] align-middle mx-auto lg:mx-0 shadow-2xl header-img"
 		/>
 	</div>
 </section>
@@ -55,5 +55,11 @@
 			theme('colors.mhPrimary') 33%,
 			theme('colors.mhSecondary') 0%
 		);
+	}
+
+	.header-img {
+		box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px,
+			rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px,
+			rgba(0, 0, 0, 0.09) 0px -3px 5px;
 	}
 </style>

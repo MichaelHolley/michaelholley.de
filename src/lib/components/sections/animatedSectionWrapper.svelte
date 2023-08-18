83d8a45db5902120d55be1a3daabb872e6993aea
Michael Holley <michael.philipp.holley@gmail.com>
2023-08-18 21:58:13 +0200

    added from bottom animation

---
+++ src/lib/components/sections/animatedSectionWrapper.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import viewport from '../../hooks/useViewportAction';
+
+	let isVisible: boolean;
+</script>
+
+<div
+	use:viewport
+	on:enterViewport={() => {
+		isVisible = true;
+	}}
+	on:exitViewport={() => {
+		isVisible = false;
+	}}
+	class:animate-in={true}
+	class:visible={isVisible}
+>
+	<slot />
+</div>
+
+<style>
+	.animate-in {
+		opacity: 0;
+		transform: translateY(30px);
+		transition: opacity 1.5s, transform 1.5s;
+	}
+
+	.visible {
+		opacity: 1;
+		transform: translateY(0);
+	}
+</style>
--- src/lib/components/sections/dev.svelte
+++ src/lib/components/sections/dev.svelte
@@ -1,39 +1,45 @@
+<script>
+	import AnimatedSectionWrapper from './animatedSectionWrapper.svelte';
+</script>
+
 <section id="dev" class="dev-bg p-8 md:p-16">
-	<div class="flex flex-row justify-between">
-		<div class="lg:w-1/3 sm:px-7 my-8 lg:my-0">
-			<h2 class="text-4xl font-bold mb-1 text-start pb-1">Backend</h2>
-			<p class="text-justify">
-				Ich habe eine Leidenschaft für das Entwerfen und Erstellen der Architektur eines Systems,
-				das die Funktionalität einer Anwendung bereit stellt. Besonders interessant finde ich die
-				Aspekte Performance sowie Skalierbarkeit. Das Arbeiten an komplexen Problemen und das Finden
-				von Lösungen rund um Daten und deren Verarbeitung faszinieren mich.
-			</p>
+	<AnimatedSectionWrapper>
+		<div class="flex flex-row justify-between">
+			<div class="lg:w-1/3 sm:px-7 my-8 lg:my-0">
+				<h2 class="text-4xl font-bold mb-1 text-start pb-1">Backend</h2>
+				<p class="text-justify">
+					Ich habe eine Leidenschaft für das Entwerfen und Erstellen der Architektur eines Systems,
+					das die Funktionalität einer Anwendung bereit stellt. Besonders interessant finde ich die
+					Aspekte Performance sowie Skalierbarkeit. Das Arbeiten an komplexen Problemen und das
+					Finden von Lösungen rund um Daten und deren Verarbeitung faszinieren mich.
+				</p>
+			</div>
+			<div class="hidden lg:block min-h-full w-1/5 border-r-8 border-t-8 border-mhSecondary" />
 		</div>
-		<div class="hidden lg:block min-h-full w-1/5 border-r-8 border-t-8 border-mhSecondary" />
-	</div>
-	<div class="flex flex-row justify-center">
-		<div class="lg:w-1/3 sm:px-7 my-8 lg:my-0">
-			<h2 class="text-4xl font-bold mb-1 text-start pb-1">Frontend</h2>
-			<p class="text-justify">
-				Insgesamt bin ich von der Frontend-Entwicklung begeistert, weil sie es mir ermöglicht,
-				schöne, benutzerfreundliche Designs zu erstellen, mit anderen Experten in diesem Bereich
-				zusammenzuarbeiten, mit den neuesten Webtechnologien zu arbeiten, sofortiges Feedback zu
-				erhalten und als Entwickler ständig zu lernen und zu wachsen.
-			</p>
+		<div class="flex flex-row justify-center">
+			<div class="lg:w-1/3 sm:px-7 my-8 lg:my-0">
+				<h2 class="text-4xl font-bold mb-1 text-start pb-1">Frontend</h2>
+				<p class="text-justify">
+					Insgesamt bin ich von der Frontend-Entwicklung begeistert, weil sie es mir ermöglicht,
+					schöne, benutzerfreundliche Designs zu erstellen, mit anderen Experten in diesem Bereich
+					zusammenzuarbeiten, mit den neuesten Webtechnologien zu arbeiten, sofortiges Feedback zu
+					erhalten und als Entwickler ständig zu lernen und zu wachsen.
+				</p>
+			</div>
 		</div>
-	</div>
-	<div class="flex flex-row justify-end">
-		<div class="lg:w-1/3 sm:px-7 my-8 lg:my-0">
-			<h2 class="text-4xl font-bold mb-1 text-start pb-1">DevOps</h2>
-			<p class="text-justify">
-				Ich arbeite gerne im Bereich DevOps, weil hier der Einsatz von Automatisierungstools und
-				-technologien gefördert wird, die dazu beitragen können, menschliche Fehler zu reduzieren,
-				die Effizienz zu steigern und die Softwarequalität insgesamt zu verbessern. Ich liebe es,
-				sich wiederholende Aufgaben zu automatisieren und dadurch mehr Zeit für spannendere Projekte
-				zu gewinnen.
-			</p>
-		</div>
-	</div>
+		<div class="flex flex-row justify-end">
+			<div class="lg:w-1/3 sm:px-7 my-8 lg:my-0">
+				<h2 class="text-4xl font-bold mb-1 text-start pb-1">DevOps</h2>
+				<p class="text-justify">
+					Ich arbeite gerne im Bereich DevOps, weil hier der Einsatz von Automatisierungstools und
+					-technologien gefördert wird, die dazu beitragen können, menschliche Fehler zu reduzieren,
+					die Effizienz zu steigern und die Softwarequalität insgesamt zu verbessern. Ich liebe es,
+					sich wiederholende Aufgaben zu automatisieren und dadurch mehr Zeit für spannendere
+					Projekte zu gewinnen.
+				</p>
+			</div>
+		</div></AnimatedSectionWrapper
+	>
 </section>
 
 <style>

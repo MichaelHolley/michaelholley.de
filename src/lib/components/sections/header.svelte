5e0551250958d4d8bbf624a405c941ab278075ea
Michael Holley <michael.philipp.holley@gmail.com>
2023-08-29 20:54:09 +0200

    removing animations to the requiry of JS

--- src/lib/components/sections/header.svelte
+++ src/lib/components/sections/header.svelte
@@ -8,49 +8,47 @@
 </script>
 
 <section class="header-bg">
-	<AnimatedSectionWrapper from="top">
-		<div class="flex flex-col justify-between lg:flex-row md:pl-16">
-			<div class="px-8 md:px-7 pt-10 lg:pt-0 flex flex-col justify-center ">
-				<h1 class="text-5xl sm:text-8xl font-bold">Michael</h1>
-				<h1 class="text-5xl sm:text-8xl font-bold">Holley</h1>
-				<h2 class="text-xl pl-1 mt-6">Fullstack-Developer</h2>
-				<div class="h-11 flex flex-row mt-6 pl-1">
-					<a
-						class="hover:scale-110 transition-all"
-						href="https://www.linkedin.com/in/michael-holley-791a64228/"
-						target="_blank"
-						rel="noreferrer"
-						aria-label="LinkedIn-Profile"
-					>
-						<LinkedinIcon color={colors.white} height={45} />
-					</a>
-					<a
-						class="ml-3 hover:scale-110 transition-all"
-						href="https://github.com/MichaelHolley"
-						target="_blank"
-						rel="noreferrer"
-						aria-label="GitHub-Profile"
-					>
-						<GithubIcon color={colors.white} height={45} />
-					</a>
-					<a
-						class="ml-3 hover:scale-110 transition-all"
-						href="mailto:michael.philipp.holley@gmail.com"
-						aria-label="GitHub-Profile"
-					>
-						<MailIcon color={colors.white} height={45} />
-					</a>
-				</div>
-			</div>
-			<div class="p-10 lg:pr-52 flex flex-col justify-center">
-				<img
-					src={MeImg}
-					alt="Michael Holley"
-					class="w-[250px] lg:w-[350px] align-middle mx-auto lg:mx-0 shadow-2xl header-img"
-				/>
+	<div class="flex flex-col justify-between lg:flex-row md:pl-16">
+		<div class="px-8 md:px-7 pt-10 lg:pt-0 flex flex-col justify-center ">
+			<h1 class="text-5xl sm:text-8xl font-bold">Michael</h1>
+			<h1 class="text-5xl sm:text-8xl font-bold">Holley</h1>
+			<h2 class="text-xl pl-1 mt-6">Fullstack-Developer</h2>
+			<div class="h-11 flex flex-row mt-6 pl-1">
+				<a
+					class="hover:scale-110 transition-all"
+					href="https://www.linkedin.com/in/michael-holley-791a64228/"
+					target="_blank"
+					rel="noreferrer"
+					aria-label="LinkedIn-Profile"
+				>
+					<LinkedinIcon color={colors.white} height={45} />
+				</a>
+				<a
+					class="ml-3 hover:scale-110 transition-all"
+					href="https://github.com/MichaelHolley"
+					target="_blank"
+					rel="noreferrer"
+					aria-label="GitHub-Profile"
+				>
+					<GithubIcon color={colors.white} height={45} />
+				</a>
+				<a
+					class="ml-3 hover:scale-110 transition-all"
+					href="mailto:michael.philipp.holley@gmail.com"
+					aria-label="GitHub-Profile"
+				>
+					<MailIcon color={colors.white} height={45} />
+				</a>
 			</div>
 		</div>
-	</AnimatedSectionWrapper>
+		<div class="p-10 lg:pr-52 flex flex-col justify-center">
+			<img
+				src={MeImg}
+				alt="Michael Holley"
+				class="w-[250px] lg:w-[350px] align-middle mx-auto lg:mx-0 shadow-2xl header-img"
+			/>
+		</div>
+	</div>
 </section>
 
 <style>

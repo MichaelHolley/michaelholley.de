64170900a35070fb106f69a0a68bc6e5f89e3df4
Michael Holley <michael.philipp.holley@gmail.com>
2024-10-24 20:56:31 +0200

    minor updates

--- src/lib/components/career/step.svelte
+++ src/lib/components/career/step.svelte
@@ -4,16 +4,19 @@
 	export let workplace: string;
 </script>
 
-<div class="mx-auto py-7 pl-8 border-l-4 relative">
-	<span class="block">{workplace}</span>
+<div class="mx-auto py-4 pl-8 border-l-2 border-neutral-500 relative">
+	<span class="inline-flex gap-2 text-base text-neutral-500">
+		<span>{workplace}</span>
+		<span class="text-neutral-300">|</span>
+		<span>{period}</span>
+	</span>
 	<div class="relative">
-		<div class="flex flex-col md:flex-row justify-between gap-1 md:gap-3">
-			<h4 class="text-2xl font-bold">{title}</h4>
-			<span class="block text-tertiary text-base md:text-lg">{period}</span>
-		</div>
-		<div class="h-5 w-5 bg-black rounded-full absolute top-[50%] -translate-y-[50%] -left-[44px]" />
+		<h4 class="text-2xl font-bold">{title}</h4>
+		<div
+			class="h-4 w-4 bg-neutral-500 rounded-full absolute top-[50%] -translate-y-1/2 -left-[41px]"
+		/>
 	</div>
-	<span class="block mt-2">
+	<span class="block mt-1 text-lg">
 		<slot />
 	</span>
 </div>
--- src/lib/components/sections/dev.svelte
+++ src/lib/components/sections/dev.svelte
@@ -1,7 +1,7 @@
 <section id="dev">
 	<div class="container py-12 md:py-20 flex flex-col lg:flex-row gap-8 lg:gap-12 xl:gap-16">
 		<div>
-			<h2 class="text-2xl tracking-widest mb-2">Backend</h2>
+			<h2 class="text-2xl tracking-widest mb-1 text-neutral-500">Backend</h2>
 			<p class="text-justify">
 				Ich habe eine Leidenschaft für das Entwerfen und Erstellen der Architektur eines Systems,
 				das die Funktionalität einer Anwendung bereit stellt. Besonders interessant finde ich die
@@ -10,7 +10,7 @@
 			</p>
 		</div>
 		<div>
-			<h2 class="text-2xl tracking-widest mb-2">Frontend</h2>
+			<h2 class="text-2xl tracking-widest mb-1 text-neutral-500">Frontend</h2>
 			<p class="text-justify">
 				Insgesamt bin ich von der Frontend-Entwicklung begeistert, weil sie es mir ermöglicht,
 				schöne, benutzerfreundliche Designs zu erstellen, mit anderen Experten in diesem Bereich
@@ -19,7 +19,7 @@
 			</p>
 		</div>
 		<div>
-			<h2 class="text-2xl tracking-widest mb-2">DevOps</h2>
+			<h2 class="text-2xl tracking-widest mb-1 text-neutral-500">DevOps</h2>
 			<p class="text-justify">
 				Ich arbeite gerne im Bereich DevOps, weil hier der Einsatz von Automatisierungstools und
 				-technologien gefördert wird, die dazu beitragen können, menschliche Fehler zu reduzieren,

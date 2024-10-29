86623e671883259f03e08fbf215fb990abe26519
Michael Holley <michael.philipp.holley@gmail.com>
2024-10-29 13:15:56 +0100

    devsection spacing

--- src/lib/components/sections/DevSection.svelte
+++ src/lib/components/sections/DevSection.svelte
@@ -1,8 +1,8 @@
 <section id="dev">
 	<div class="container py-12 md:py-20 grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-12 xl:gap-16">
 		<div>
-			<h2 class="text-2xl tracking-widest mb-1 text-neutral-500">Backend</h2>
-			<p class="text-justify">
+			<h2 class="text-2xl tracking-widest mb-2 text-neutral-500">Backend</h2>
+			<p class="leading-7">
 				Als Backend-Entwickler fasziniert mich die Logik hinter den Kulissen. Die Arbeit mit
 				Datenbanken und APIs, um leistungsstarke und skalierbare Anwendungen zu bauen, ist für mich
 				pure Leidenschaft. Ich liebe es, effiziente Lösungen zu entwickeln und komplexe Probleme zu
@@ -10,8 +10,8 @@
 			</p>
 		</div>
 		<div>
-			<h2 class="text-2xl tracking-widest mb-1 text-neutral-500">Frontend</h2>
-			<p class="text-justify">
+			<h2 class="text-2xl tracking-widest mb-2 text-neutral-500">Frontend</h2>
+			<p class="leading-7">
 				Die Frontend-Entwicklung ist für mich eine spannende Herausforderung. Ich beschäftige mich
 				gerne mit den neuesten Technologien und Frameworks, um performante und skalierbare
 				Webanwendungen zu bauen. Die Freiheit, meine Kreativität auszuleben und gleichzeitig
@@ -19,8 +19,8 @@
 			</p>
 		</div>
 		<div>
-			<h2 class="text-2xl tracking-widest mb-1 text-neutral-500">DevOps</h2>
-			<p class="text-justify">
+			<h2 class="text-2xl tracking-widest mb-2 text-neutral-500">DevOps</h2>
+			<p class="leading-7">
 				DevOps vereint für mich das Beste aus beiden Welten: Entwicklung und Betrieb. Ich finde es
 				faszinierend, Prozesse zu automatisieren und die Zusammenarbeit zwischen Entwicklern und
 				Operations-Teams zu optimieren. Durch DevOps können wir schnellere und zuverlässigere

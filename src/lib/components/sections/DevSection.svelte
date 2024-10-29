47ee556e7ad5e36c400aef047aed379631364bbf
Michael Holley <michael.philipp.holley@gmail.com>
2024-10-29 13:38:38 +0100

    highlighting dev text

--- src/lib/components/sections/DevSection.svelte
+++ src/lib/components/sections/DevSection.svelte
@@ -4,27 +4,29 @@
 			<h2 class="text-2xl tracking-widest mb-2 text-neutral-500">Backend</h2>
 			<p class="leading-7">
 				Als Backend-Entwickler fasziniert mich die Logik hinter den Kulissen. Die Arbeit mit
-				Datenbanken und APIs, um leistungsstarke und skalierbare Anwendungen zu bauen, ist für mich
-				pure Leidenschaft. Ich liebe es, effiziente Lösungen zu entwickeln und komplexe Probleme zu
-				lösen.
+				Datenbanken und APIs, um <span class="text-secondary"
+					>leistungsstarke und skalierbare Anwendungen</span
+				> zu bauen, ist für mich pure Leidenschaft. Ich liebe es, effiziente Lösungen zu entwickeln und
+				komplexe Probleme zu lösen.
 			</p>
 		</div>
 		<div>
 			<h2 class="text-2xl tracking-widest mb-2 text-neutral-500">Frontend</h2>
 			<p class="leading-7">
 				Die Frontend-Entwicklung ist für mich eine spannende Herausforderung. Ich beschäftige mich
-				gerne mit den neuesten Technologien und Frameworks, um performante und skalierbare
-				Webanwendungen zu bauen. Die Freiheit, meine Kreativität auszuleben und gleichzeitig
-				technisch anspruchsvolle Probleme zu lösen, begeistert mich.
+				gerne mit den <span class="text-secondary">neuesten Technologien und Frameworks</span>, um
+				performante und skalierbare Webanwendungen zu bauen. Die Freiheit, meine
+				<span class="text-secondary">Kreativität</span> auszuleben und gleichzeitig technisch anspruchsvolle
+				Probleme zu lösen, begeistert mich.
 			</p>
 		</div>
 		<div>
 			<h2 class="text-2xl tracking-widest mb-2 text-neutral-500">DevOps</h2>
 			<p class="leading-7">
 				DevOps vereint für mich das Beste aus beiden Welten: Entwicklung und Betrieb. Ich finde es
-				faszinierend, Prozesse zu automatisieren und die Zusammenarbeit zwischen Entwicklern und
-				Operations-Teams zu optimieren. Durch DevOps können wir schnellere und zuverlässigere
-				Software-Releases ermöglichen.
+				faszinierend, Prozesse zu <span class="text-secondary">automatisieren</span> und die
+				Zusammenarbeit zwischen Entwicklern und Operations-Teams zu optimieren. Durch DevOps können
+				wir <span class="text-secondary">schnellere und zuverlässigere Software-Releases</span> ermöglichen.
 			</p>
 		</div>
 	</div>

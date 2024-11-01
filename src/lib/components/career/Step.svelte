d73fa61f188e35af15fdc91f029e224f0f8e9f4f
Michael Holley <michael.philipp.holley@gmail.com>
2024-11-01 10:53:20 +0100

    minor changes

--- src/lib/components/career/Step.svelte
+++ src/lib/components/career/Step.svelte
@@ -7,7 +7,7 @@
 <div class="mx-auto py-4 relative">
 	<span class="inline-flex gap-2 text-base text-neutral-500">
 		<span>{workplace}</span>
-		<span class="text-neutral-300">|</span>
+		<span class="text-neutral-300">//</span>
 		<span>{period}</span>
 	</span>
 	<h4 class="text-xl sm:text-2xl font-bold">{title}</h4>

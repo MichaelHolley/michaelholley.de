5773dc831cd3fc39f48b3f0b9de1b350592aa687
Michael Holley <michael.philipp.holley@gmail.com>
2024-11-20 19:24:59 +0100

    wip

--- src/lib/components/shared/TechStackListing.svelte
+++ src/lib/components/shared/TechStackListing.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
 	import { cn } from '$lib/utils';
 	import Icon from '@iconify/svelte';
-	import BlurFade from './misc/BlurFade.svelte';
 
 	export let icons: string[] = [];
 </script>
 
 <div class={cn('flex flex-row items-center gap-2 overflow-hidden', $$props.class)}>
 	{#each icons as icon, i}
-		<BlurFade delay={0.15 * i} once={false}>
-			<Icon {icon} /></BlurFade
-		>
+		<Icon {icon} />
 	{/each}
 </div>

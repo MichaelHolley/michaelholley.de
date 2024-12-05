f566b713ba1e7d13165d20c10c71d8bfc7e13129
Michael Holley <michael.philipp.holley@gmail.com>
2024-12-05 19:35:00 +0100

    wip

--- src/lib/components/sections/ProjectsSection.svelte
+++ src/lib/components/sections/ProjectsSection.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+	import { cn } from '$lib/utils';
 	import { projects } from '../shared/data/projects';
 	import BentoCard from '../shared/misc/BentoCard.svelte';
 	import BentoGrid from '../shared/misc/BentoGrid.svelte';
 	import SectionHeader from '../shared/SectionHeader.svelte';
+
+	let selectedIndex = -1;
 </script>
 
 <section id="projects" class="bg-black text-white">
@@ -10,13 +13,18 @@
 		<SectionHeader title="Projekte" class="pb-8" />
 		<div class="flex flex-col justify-center items-center gap-24 md:gap-64">
 			<BentoGrid>
-				{#each projects as project}
+				{#each projects as project, i}
 					<BentoCard
 						name={project.title}
 						description={project.shortDescription}
 						icon={project.icon}
-						class={project.class}
+						class={cn(project.class, selectedIndex === i && '!col-span-full row-span-2')}
 						cta="Learn more"
+						selected={selectedIndex === i}
+						on:click={() => {
+							if (selectedIndex === i) selectedIndex = -1;
+							else selectedIndex = i;
+						}}
 					/>
 				{/each}
 			</BentoGrid>
--- src/lib/components/shared/misc/BentoCard.svelte
+++ src/lib/components/shared/misc/BentoCard.svelte
@@ -2,6 +2,7 @@
 	import { cn } from '$lib/utils';
 	import Icon from '@iconify/svelte';
 	import { iconMap } from '../data/icons';
+	import { createEventDispatcher } from 'svelte';
 
 	export let name;
 	export let background: any;
@@ -9,16 +10,16 @@
 	export let description;
 	export let href: string;
 	export let cta;
+	export let selected: boolean;
+
+	const dispatch = createEventDispatcher();
 </script>
 
 <div
 	id={name}
 	class={cn(
 		'group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl',
-		// light styles
-		'bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]',
-		// dark styles
-		'transform-gpu dark:bg-black dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]',
+		'transform-gpu bg-black dark:[border:1px_solid_rgba(255,255,255,.1)] [box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]',
 		$$props.class
 	)}
 >
@@ -26,11 +27,11 @@
 		<svelte:component this={background} />
 	</div>
 	<div
-		class="pointer-events-none z-10 flex transform-gpu flex-col gap-0.5 p-6 transition-all duration-300 group-hover:-translate-y-10"
+		class="pointer-events-none z-10 flex transform-gpu flex-col gap-0.5 p-6 pb-4 transition-all duration-300 group-hover:-translate-y-10"
 	>
 		<div class="flex flex-row items-center gap-2 text-xl">
 			{#if !!icon}
-				<Icon {icon} class="text-neutral-700 text-2xl group-hover:text-xl transition-all -m-1" />
+				<Icon {icon} class="text-neutral-700 text-2xl group-hover:text-xl transition-all -ml-0.5" />
 			{/if}
 
 			<h3 class="font-semibold text-neutral-700 dark:text-neutral-300">
@@ -45,7 +46,7 @@
 			'pointer-events-none absolute bottom-0 flex flex-grow translate-y-10 transform-gpu flex-row items-center p-6 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100'
 		)}
 	>
-		<button class="pointer-events-auto">
+		<button class="pointer-events-auto" on:click={() => dispatch('click')}>
 			<a {href} class="flex justify-center items-center">
 				{cta}
 				<Icon icon={iconMap.arrowright} class="ml-0.5" />

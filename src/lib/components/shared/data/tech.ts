3e8b36a7c4faa01676d88d5d80a9069aace1e560
Michael Holley <michael.philipp.holley@gmail.com>
2024-12-06 13:15:41 +0100

    tech cleanup

--- src/lib/components/shared/data/tech.ts
+++ src/lib/components/shared/data/tech.ts
@@ -3,77 +3,81 @@ import colors from 'tailwindcss/colors';
 export const tech: Tech[] = [
 	{
 		icon: 'devicon:dot-net',
-		color: colors.indigo[600],
-		label: '.NET'
+		color: colors.indigo[600]
 	},
 	{
-		label: 'Java',
 		icon: 'devicon:java',
 		color: colors.red[600]
 	},
 	{
-		label: 'HTML',
 		icon: 'devicon:html5',
 		color: colors.orange[600]
 	},
 	{
 		icon: 'devicon:typescript',
-		color: colors.sky[600],
-		label: 'TypeScript'
+		color: colors.sky[600]
 	},
 	{
 		icon: 'devicon:tailwindcss',
-		color: colors.sky[400],
-		label: 'TailwindCSS'
+		color: colors.sky[400]
 	},
 	{
 		icon: 'devicon:svelte',
-		color: colors.orange[600],
-		label: 'Svelte'
+		color: colors.orange[600]
 	},
 	{
 		icon: 'devicon:react',
-		color: colors.cyan[300],
-		label: 'React'
+		color: colors.cyan[300]
 	},
-	{ label: 'Angular', icon: 'devicon:angular', color: colors.fuchsia[500] },
-	{ label: 'Vue', icon: 'devicon:vuejs', color: colors.emerald[400] },
+	{
+		icon: 'devicon:angular',
+		color: colors.fuchsia[500]
+	},
+	{
+		icon: 'devicon:vuejs',
+		color: colors.emerald[400]
+	},
+	{
+		icon: 'devicon:css3',
+		color: colors.sky[400]
+	},
+	{
+		icon: 'devicon:nodejs',
+		color: colors.green[600]
+	},
 	{
 		icon: 'devicon:python',
-		color: colors.yellow[300],
-		label: 'Python'
+		color: colors.yellow[300]
 	},
 	{
-		label: 'GitLab',
 		icon: 'devicon:gitlab',
 		color: colors.orange[500]
 	},
 	{
-		label: 'Docker',
 		icon: 'devicon:docker',
 		color: colors.cyan[500]
 	},
 	{
-		label: 'GitHub',
 		icon: 'devicon:github',
 		invertIconColor: true,
 		color: colors.white
 	},
 	{
-		label: 'MongoDB',
 		icon: 'devicon:mongodb',
 		color: colors.green[600]
 	},
 	{
-		label: 'MySQL',
 		icon: 'devicon:mysql',
 		color: colors.sky[800]
+	},
+	{
+		icon: 'devicon:git',
+		color: colors.red[500]
 	}
 ];
 
 interface Tech {
 	icon: string;
 	color: string;
-	label: string;
 	invertIconColor?: boolean;
 }

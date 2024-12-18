5e434c83938745cfe1ac242a5847a7d8556782f3
Michael Holley <michael.philipp.holley@gmail.com>
2024-12-18 21:00:57 +0100

    updated experience description

--- src/lib/components/sections/ExperienceSection.svelte
+++ src/lib/components/sections/ExperienceSection.svelte
@@ -8,15 +8,20 @@
 		<SectionHeader title="Experience" class="text-center" />
 		<div class="text-lg text-left">
 			<Step period="seit 2022" title="Software Developer" workplace="AraCom IT Services AG">
-				<ul class="list-square list-inside">
+				<ul class="list-square list-inside *:py-1">
 					<li>
-						teambasierte Entwicklung diverser webbasierten Dashboards und weiterer Softwarelösungen
+						Entwicklung und Optimierung webbasierter Dashboards sowie vielseitiger Softwarelösungen
+						in einem agilen, teamorientierten Umfeld
 					</li>
 					<li>
-						Rolle des Technical Leads Frontend in der Betriebsphase einer Smartphone-App im
-						Lebensmittelbereich
+						Übernahme der Rolle des Technical Leads Frontend während der Betriebsphase einer
+						innovativen Smartphone-App im Bereich Lebensmittel, einschließlich technischer
+						Verantwortung
 					</li>
-					<li>Engagierte Unterstützung bei der Ausbildung von Nachwuchskräften</li>
+					<li>
+						Aktive Mitwirkung bei der Ausbildung und Förderung von Nachwuchskräften, einschließlich
+						Mentoring und Wissensvermittlung
+					</li>
 				</ul>
 			</Step>
 			<Step
@@ -24,10 +29,15 @@
 				title="Ausbildung: Fachinformatiker für Anwendungsentwicklung"
 				workplace="AraCom IT Services AG"
 			>
-				<ul class="list-square list-inside">
-					<li>Verkürzte Ausbildung auf 2.5 Jahre</li>
-					<li>Abschluss der Berufsschule mit <strong>Note 1,0</strong></li>
-					<li>IHK Prüfung abgelegt mit <strong>96 von 100</strong> Punkten gesamt</li>
+				<ul class="list-square list-inside *:py-1">
+					<li>
+						<strong>Verkürzte Ausbildung</strong> erfolgreich in nur 2,5 Jahren abgeschlossen, dank herausragender
+						Leistungen und hoher Eigeninitiative
+					</li>
+					<li>Abschluss der Berufsschule mit <strong>Bestnote 1,0</strong></li>
+					<li>
+						IHK-Abschlussprüfung mit einem Gesamtergebnis von <strong>96 von 100 Punkten</strong> absolviert
+					</li>
 				</ul>
 			</Step>
 		</div>

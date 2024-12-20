d0be227a658aae0e83230fdfabd997e6a5299098
Michael Holley <michael.philipp.holley@gmail.com>
2024-12-20 11:35:05 +0100

    flex order
    fixed this mess with flex order which is awesome and have forgotten about

--- src/lib/components/sections/ProjectsSection.svelte
+++ src/lib/components/sections/ProjectsSection.svelte
@@ -6,18 +6,7 @@
 	import SectionHeader from '../shared/SectionHeader.svelte';
 
 	let gridDivRef: HTMLElement;
-
-	let selectedId: string | undefined = undefined;
-	const cardsMap = new Map<string, any>();
-	projects.forEach((p) => cardsMap.set(crypto.randomUUID(), p));
-
-	$: cards =
-		selectedId === undefined
-			? [...cardsMap.entries()]
-			: [
-					[selectedId, cardsMap.get(selectedId)],
-					...Array.from(cardsMap.entries()).filter(([id, c]) => selectedId != id)
-			  ];
+	let selectedIndex = -1;
 </script>
 
 <section id="projects" class="bg-black text-white">
@@ -25,22 +14,21 @@
 		<SectionHeader title="Projekte" class="pb-8" />
 		<div class="flex flex-col justify-center items-center gap-24 md:gap-64" bind:this={gridDivRef}>
 			<BentoGrid>
-				{#each cards as card, i}
+				{#each projects as card, i}
 					<BentoCard
-						name={card[1].title}
-						description={card[1].shortDescription}
-						icon={card[1].icon}
+						name={card.title}
+						description={card.shortDescription}
+						icon={card.icon}
 						class={cn(
-							card[1].class,
-							selectedId === card[0] && i === 0 && '!col-span-full row-span-2',
-							selectedId != undefined && 'col-span-full lg:col-span-1'
+							card.class,
+							selectedIndex === i && '-order-1 !col-span-full row-span-2',
+							selectedIndex != i && selectedIndex != -1 && 'col-span-1 lg:col-span-1'
 						)}
 						cta="Learn more"
-						selected={selectedId === card[0]}
+						selected={selectedIndex === i}
 						on:click={() => {
-							if (selectedId != card[0]) selectedId = card[0];
-							else selectedId = undefined;
-
+							if (selectedIndex === i) selectedIndex = -1;
+							else selectedIndex = i;
 							gridDivRef.scrollIntoView({ behavior: 'smooth' });
 						}}
 					/>

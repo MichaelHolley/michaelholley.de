1c4c7dc00e3a0d624fbe7511e3cacc65c91c25f9
Michael Holley <michael.philipp.holley@gmail.com>
2024-12-22 21:53:31 +0100

    footer hover scale

--- src/lib/components/sections/Footer.svelte
+++ src/lib/components/sections/Footer.svelte
@@ -16,7 +16,7 @@
 				class="flex flex-row sm:flex-col flex-wrap justify-center sm:items-start gap-x-10 sm:gap-x-0 gap-y-3"
 			>
 				<a
-					class="flex flex-row gap-2 justify-start sm:justify-center group items-center leading-9"
+					class="flex flex-row gap-2 justify-start sm:justify-center hover:scale-105 transition-all items-center leading-9"
 					href="mailto:michael.philipp.holley@gmail.com"
 					aria-label="GitHub-Profile"
 				>
@@ -26,7 +26,7 @@
 					Contact me
 				</a>
 				<a
-					class="flex flex-row gap-2 justify-start sm:justify-center group items-center leading-9"
+					class="flex flex-row gap-2 justify-start sm:justify-center hover:scale-105 transition-all items-center leading-9"
 					href="https://github.com/MichaelHolley"
 					target="_blank"
 					rel="noreferrer"
@@ -38,7 +38,7 @@
 					GitHub
 				</a>
 				<a
-					class="flex flex-row gap-2 justify-start sm:justify-center group items-center leading-9"
+					class="flex flex-row gap-2 justify-start sm:justify-center hover:scale-105 transition-all items-center leading-9"
 					href="https://www.linkedin.com/in/michael-holley-791a64228/"
 					target="_blank"
 					rel="noreferrer"

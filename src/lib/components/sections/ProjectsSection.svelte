1cb714d6ec952d5920e7104f96bb98330513daba
Michael Holley <michael.philipp.holley@gmail.com>
2025-02-02 18:20:59 +0100

    wip bento grid

--- src/lib/components/sections/ProjectsSection.svelte
+++ src/lib/components/sections/ProjectsSection.svelte
@@ -1,77 +1,28 @@
 <script lang="ts">
-	import BlurFade from '$lib/components/shared/misc/BlurFade.svelte';
 	import SectionHeader from '$lib/components/shared/SectionHeader.svelte';
 	import type { Project } from '$lib/server/projects';
-	import { cn } from '$lib/utils';
-	import Icon from '@iconify/svelte';
+	import BentoCardComponent from '../shared/BentoGrid/BentoCardComponent.svelte';
+	import BentoGridComponent from '../shared/BentoGrid/BentoGridComponent.svelte';
 
 	const { projects }: { projects: Project[] } = $props();
-	let selectedIndex = $state(-1);
-
-	function selectProject(index: number) {
-		selectedIndex = index;
-	}
 </script>
 
-<section id="projects" class="bg-tertiary text-white">
+<section id="projects" class="bg-black text-white">
 	<div class="container py-8 md:py-16">
-		<SectionHeader title="Projekte" class="text-center md:text-start" />
-		<div class="mt-6 flex flex-col gap-4 md:flex-row">
-			<div>
-				{#each projects as p, i}
-					<BlurFade delay={0.08 * i}>
-						<button
-							class="group mb-1 w-full transition-all hover:scale-[1.03]"
-							tabindex="0"
-							onclick={() => selectProject(i)}
-							onkeyup={(e) => {
-								if (e.key == 'Enter') selectProject(i);
-							}}
-						>
-							<h4
-								class="text-center text-2xl font-bold uppercase transition-all duration-200 md:text-start lg:text-3xl xl:text-4xl {i ===
-								selectedIndex
-									? 'text-primary'
-									: 'text-white'}"
-							>
-								{p.title}
-							</h4>
-						</button>
-					</BlurFade>
+		<SectionHeader title="Projekte" class="pb-8" />
+		<div class="flex flex-col items-center justify-center gap-24 md:gap-64">
+			<BentoGridComponent class="grid-cols-1 lg:grid-cols-3">
+				{#each projects as card}
+					<BentoCardComponent
+						name={card.title}
+						description={card.short}
+						icon={card.icon}
+						href={''}
+						cta={'Read more'}
+						class={card.class}
+					/>
 				{/each}
-			</div>
-			<div class={cn('h-[380px] w-full', selectedIndex != -1 ? 'overflow-y-auto pr-1' : '')}>
-				{#if selectedIndex >= 0 && selectedIndex < projects.length}
-					{#each projects as project, index}
-						{#if index === selectedIndex}
-							<div class="flex flex-col">
-								<div class="pr-4 text-lg">
-									{#each project.description as descr, i}
-										<p class={cn(i != 0 ? 'mt-4' : '')}>
-											{descr}
-										</p>
-									{/each}
-								</div>
-								<div class="mt-3 flex flex-row justify-center p-2">
-									{#if !!project.github && projects[selectedIndex].github != ''}
-										<a
-											class="transition-all hover:scale-110"
-											href={project.github}
-											target="_blank"
-											rel="noreferrer"
-											aria-label="GitHub-Repository"
-										>
-											<Icon icon="fa:github" style="font-size:2.5rem" />
-										</a>
-									{/if}
-								</div>
-							</div>
-						{/if}
-					{/each}
-				{:else}
-					<h3 class="text-center text-xl">Wählen Sie ein Projekt</h3>
-				{/if}
-			</div>
+			</BentoGridComponent>
 		</div>
 	</div>
 </section>
---
+++ src/lib/components/shared/BentoGrid/BentoCardComponent.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import Icon from '@iconify/svelte';
+
+	const {
+		name,
+		background,
+		icon,
+		description,
+		href,
+		cta,
+		class: className
+	} = $props<{
+		name: string;
+		background?: any;
+		icon?: string;
+		description: string;
+		href: string;
+		cta: string;
+		class?: string;
+	}>();
+</script>
+
+<div
+	id={name}
+	class={cn(
+		'group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl',
+		// light styles
+		'bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]',
+		// dark styles
+		'transform-gpu dark:bg-black dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]',
+		className
+	)}
+>
+	<div>
+		<svelte:component this={background} />
+	</div>
+	<div
+		class="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10"
+	>
+		<div class="flex flex-row items-center gap-1 text-xl">
+			{#if !!icon}
+				<Icon
+					{icon}
+					class="-ml-0.5 text-2xl text-neutral-700 transition-all group-hover:text-neutral-500"
+				/>
+			{/if}
+
+			<h3 class="font-semibold text-neutral-700 dark:text-neutral-300">
+				{name}
+			</h3>
+		</div>
+		<p class="max-w-lg text-neutral-400">{description}</p>
+	</div>
+
+	<div
+		class={cn(
+			'pointer-events-none absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-6 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100'
+		)}
+	>
+		<a {href} class="pointer-events-auto flex items-center justify-center">
+			{cta}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="ml-2 h-4 w-4"><path d="M5 12h14" /><path d="m12 5 7 7-7 7" /></svg
+			>
+		</a>
+	</div>
+	<div
+		class="pointer-events-none absolute inset-0 transform-gpu transition-all duration-300 group-hover:bg-black/[.03] group-hover:dark:bg-neutral-800/10"
+	></div>
+</div>

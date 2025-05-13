6f9484fe7879ad542583cafcf987ef14846bb08b
Michael Holley <michael.philipp.holley@gmail.com>
2025-05-13 22:17:23 +0200

    main page sectioj

---
+++ src/lib/components/sections/BlogsSection.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import SectionHeader from '$lib/components/shared/SectionHeader.svelte';
+
+	const { blogs }: { blogs: any[] } = $props();
+</script>
+
+{#if blogs.length}
+	<section id="blogs" class="bg-tertiary text-white">
+		<div class="container py-8 md:py-16">
+			<SectionHeader title="blogs" class="pb-8 text-center" />
+			<div class="ml-1 flex flex-col gap-4">
+				{#each blogs as b}
+					<a
+						href="/blog/{b.id}"
+						class="hover:border-primary min-h-16 border-l-4 border-white/50 py-1 pl-4"
+					>
+						<p class="font-bold">{b.title}</p>
+						<p>{b.description}</p>
+					</a>
+				{/each}
+			</div>
+		</div>
+	</section>
+{/if}
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,5 +3,12 @@ import { tech } from '$lib/server/tech';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-	return { projects, tech };
+	return {
+		projects,
+		tech,
+		blogs: [
+			{ title: 'Test tile 1', description: 'Test description 1', id: 1 },
+			{ title: 'Test tile 1', description: 'Test description 1', id: 2 }
+		]
+	};
 };

c325059bd2654081ab30e5a8ec181fc8900152a3
Michael Holley <michael.philipp.holley@gmail.com>
2025-05-26 22:45:56 +0200

    removed templates

--- src/lib/server/blogs.ts
+++ src/lib/server/blogs.ts
@@ -6,21 +6,4 @@ export interface Blog {
 	tags?: string[];
 }
 
-export const blogs: Blog[] = [
-	{
-		title: 'SvelteKit: The Ultimate Guide',
-		id: 'sveltekit-ultimate-guide',
-		description:
-			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-		tags: ['Svelte', 'daisyUI', 'Themes'],
-		released: 'May 2025'
-	},
-	{
-		title: 'SvelteKit: The Ultimate Guide',
-		id: 'sveltekit-ultimate-guide',
-		description:
-			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-		tags: ['Svelte', 'daisyUI', 'Themes'],
-		released: 'May 2025'
-	}
-];
+export const blogs: Blog[] = [];

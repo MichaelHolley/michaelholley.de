a8d3baa453c7d50572d81bc9f86868c472e68728
Michael Holley <michael.philipp.holley@gmail.com>
2025-07-17 11:52:38 +0200

    minor layout improvements for mobile view

--- src/lib/components/career/Step.svelte
+++ src/lib/components/career/Step.svelte
@@ -10,13 +10,13 @@
 </script>
 
 <div class="relative mx-auto py-4">
-	<div class="xs:justify-start flex flex-row justify-between gap-2 text-base text-neutral-500">
+	<div class="xs:flex-row xs:gap-2 flex flex-col justify-start text-base text-neutral-500">
 		<div class="flex flex-row justify-start gap-2">
 			<span>{workplace}</span>
-			<span class="text-neutral-300">//</span>
+			<span class="xs:block hidden text-neutral-300">&#x2022;</span>
 		</div>
 		<div>
-			<span class="flex text-right">{period}</span>
+			<span class="xs:text-base flex text-right text-sm">{period}</span>
 		</div>
 	</div>
 	<h3 class="text-xl font-bold sm:text-2xl">{title}</h3>

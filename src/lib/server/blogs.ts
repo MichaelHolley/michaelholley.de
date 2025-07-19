a841904eb81eafd5f93d3c6474167a4c95f37bed
Michael Holley <michael.philipp.holley@gmail.com>
2025-07-19 12:17:40 +0200

    replaced with cms integration

--- src/lib/server/blogs.ts
+++ src/lib/server/blogs.ts
@@ -1,18 +1,9 @@
 export interface Blog {
+	id: number;
+	documentId: string;
 	title: string;
-	id: string;
 	description: string;
+	content: string;
 	released: string;
-	tags?: string[];
+	tags: string[];
 }
-
-export const blogs: Blog[] = [
-	{
-		id: 'install-web-app',
-		title: 'Wie du deine Web-App installierst',
-		description:
-			'Verwandeln Sie Ihre Webanwendung in ein natives App-Erlebnis. Dieser Leitfaden zeigt Ihnen Schritt für Schritt, wie Sie mit nur einer Datei Ihre Web-App installieren können.',
-		released: '2025-07-18',
-		tags: ['web', 'mobile', 'app']
-	}
-];
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,17 @@
-import { blogs } from '$lib/server/blogs';
+import { STRAPI_TOKEN, STRAPI_URL } from '$env/static/private';
+import type { Blog } from '$lib/server/blogs';
 import { projects } from '$lib/server/projects';
 import { tech } from '$lib/server/tech';
 
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
+	const res = await fetch(`${STRAPI_URL}/blogs`, {
+		headers: { Authorization: `Bearer ${STRAPI_TOKEN}` }
+	});
+
+	const { data: blogs } = (await res.json()) as { data: Blog[] };
+
 	return {
 		projects,
 		tech,

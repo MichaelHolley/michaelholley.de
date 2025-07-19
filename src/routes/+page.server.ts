58d0a1e108c98a98bfac94b241072df026753bf1
Michael Holley <michael.philipp.holley@gmail.com>
2025-07-19 12:42:35 +0200

    fix build and env vars

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,4 @@
-import { STRAPI_TOKEN, STRAPI_URL } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import type { Blog } from '$lib/server/blogs';
 import { projects } from '$lib/server/projects';
 import { tech } from '$lib/server/tech';
@@ -6,8 +6,8 @@ import { tech } from '$lib/server/tech';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-	const res = await fetch(`${STRAPI_URL}/blogs`, {
-		headers: { Authorization: `Bearer ${STRAPI_TOKEN}` }
+	const res = await fetch(`${env.STRAPI_URL}/blogs`, {
+		headers: { Authorization: `Bearer ${env.STRAPI_TOKEN}` }
 	});
 
 	const { data: blogs } = (await res.json()) as { data: Blog[] };
--- src/routes/blogs/[id]/+page.server.ts
+++ src/routes/blogs/[id]/+page.server.ts
@@ -1,9 +1,9 @@
-import { STRAPI_URL, STRAPI_TOKEN } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import type { Blog } from '$lib/server/blogs';
 
 export const load = async ({ params }: { params: { id: string } }) => {
-	const res = await fetch(`${STRAPI_URL}/blogs/${params.id}`, {
-		headers: { Authorization: `Bearer ${STRAPI_TOKEN}` }
+	const res = await fetch(`${env.STRAPI_URL}/blogs/${params.id}`, {
+		headers: { Authorization: `Bearer ${env.STRAPI_TOKEN}` }
 	});
 
 	const { data: blog } = (await res.json()) as { data: Blog };

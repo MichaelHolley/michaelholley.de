47f1785f581625c819db5a8328e1618863f3f69c
Michael Holley <michael.philipp.holley@gmail.com>
2025-07-20 09:56:37 +0200

    removed logging messages

--- src/routes/blogs/[id]/+page.server.ts
+++ src/routes/blogs/[id]/+page.server.ts
@@ -4,15 +4,9 @@ import type { Blog } from '$lib/server/blogs';
 
 export const load = async ({ params }: { params: { id: string } }) => {
 	try {
-		console.log(
-			`Requesting ${env.STRAPI_URL}/blogs/${params.id} - with: ${env.STRAPI_TOKEN.substring(0, 5)}`
-		);
-
 		const res = await fetch(`${env.STRAPI_URL}/blogs/${params.id}`, {
 			headers: { Authorization: `Bearer ${env.STRAPI_TOKEN}` }
 		});
-
-		console.log(res);
 
 		const { data: blog } = (await res.json()) as { data: Blog };
 

867c8806eb6387e7660925e7bce4ccd07f2574a0
Michael Holley <michael.philipp.holley@gmail.com>
2025-11-02 16:08:52 +0100

    implemented TextInput Component

---
+++ src/lib/components/shared/input/TextInputComponent.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	let {
+		value = $bindable(''),
+		id,
+		name,
+		type = 'text',
+		placeholder = '',
+		required = false,
+		class: className
+	} = $props<{
+		id: string;
+		name: string;
+		value: string;
+		type?: 'text' | 'email' | 'textarea';
+		placeholder?: string;
+		required?: boolean;
+	}>();
+</script>
+
+{#if type !== 'textarea'}
+	<input
+		bind:value
+		{type}
+		{id}
+		{name}
+		{placeholder}
+		{required}
+		class={`border-b border-neutral-300 focus:outline-none active:outline-none ${className} w-full px-1`}
+	/>
+{:else}
+	<textarea
+		bind:value
+		{id}
+		{name}
+		{placeholder}
+		{required}
+		class={`bg-neutral-200 focus:outline-none active:outline-none ${className} w-full p-1`}
+	>
+	</textarea>
+{/if}
+
+<style>
+	textarea::placeholder,
+	input::placeholder {
+		font-size: 0.7rem;
+	}
+</style>

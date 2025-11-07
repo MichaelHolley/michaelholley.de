4b7298dbdf14adbcb7eae1731c1fb093eb479365
Michael Holley <michael.philipp.holley@gmail.com>
2025-11-07 16:52:03 +0100

    feat(Client): minor change to icons in dev section

--- src/lib/components/dev/DevHeaderComponent.svelte
+++ src/lib/components/dev/DevHeaderComponent.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { Tech } from '$lib/server/tech';
+	import { cn } from '$lib/utils';
 	import Icon from '@iconify/svelte';
 
 	const { header, tech } = $props<{ header: string; tech?: Tech[] }>();
@@ -9,10 +10,10 @@
 	<h3 class="text-2xl tracking-widest text-neutral-500">{header}</h3>
 	{#if !!tech}
 		<div class="flex flex-row items-center gap-2">
-			{#each tech as t}
+			{#each tech as t, i}
 				<Icon
 					icon={t.icon}
-					class="text-xl saturate-0 transition-all duration-300 hover:saturate-100"
+					class={'text-xl saturate-0 transition-all duration-300 hover:rotate-8 hover:saturate-100'}
 				/>
 			{/each}
 		</div>

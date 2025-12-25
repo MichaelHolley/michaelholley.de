8fd0ee69e33c4e38db964252954f70af9fff69c9
Michael Holley <michael.philipp.holley@gmail.com>
2025-12-25 09:55:06 +0100

    feat: scroll to top button
    on content pages

--- src/lib/components/shared/ContentPageComponent.svelte
+++ src/lib/components/shared/ContentPageComponent.svelte
@@ -2,7 +2,9 @@
 	import { serifStore } from '$lib/stores/serifFontStore';
 	import { cn } from '$lib/utils';
 	import Icon from '@iconify/svelte';
-	import type { Snippet } from 'svelte';
+	import { ScrollState } from 'runed';
+	import { type Snippet } from 'svelte';
+	import { browser } from '$app/environment';
 
 	const {
 		children,
@@ -13,6 +15,24 @@
 	} = $props();
 
 	const serifFont = serifStore;
+
+	let el = $state<Window | undefined>();
+
+	const scroll = new ScrollState({
+		element: () => el,
+		behavior: 'smooth'
+	});
+
+	$effect(() => {
+		if (browser) {
+			el = window;
+		}
+	});
+
+	const buttonVariant = cn(
+		'text-neutral-400 hover:text-neutral-500',
+		'dark:text-neutral-700 hover:dark:text-neutral-600'
+	);
 </script>
 
 <div class="bg-white text-black dark:bg-transparent dark:text-white">
@@ -29,8 +49,9 @@
 				</button>
 				<button
 					class={cn(
-						'size-8 rounded-sm border hover:cursor-pointer',
-						!serifFont.current && 'font-serif'
+						'size-8 rounded-sm border transition-all hover:cursor-pointer',
+						!serifFont.current && 'font-serif',
+						buttonVariant
 					)}
 					onclick={() => {
 						serifFont.current = !serifFont.current;
@@ -47,5 +68,19 @@
 				</div>
 			</div>
 		</div>
+
+		{#if scroll.y > 90}
+			<div class="motion-translate-y-in-150 fixed bottom-3 left-3 z-40">
+				<button
+					class={cn(
+						'flex size-12 items-center justify-center transition-all hover:cursor-pointer',
+						buttonVariant
+					)}
+					onclick={() => scroll.scrollToTop()}
+				>
+					<Icon icon="ic:baseline-arrow-back" class="rotate-90 text-2xl" />
+				</button>
+			</div>
+		{/if}
 	</div>
 </div>

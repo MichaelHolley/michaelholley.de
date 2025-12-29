498fe586a790632b5314e132f1fe6093c2e015ad
Michael Holley <michael.philipp.holley@gmail.com>
2025-12-29 15:24:35 +0100

    feat: devtools added

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+import devtoolsJson from 'vite-plugin-devtools-json';
 import { defineConfig } from 'vitest/config';
 import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), devtoolsJson()],
 
 	test: {
 		expect: { requireAssertions: true },

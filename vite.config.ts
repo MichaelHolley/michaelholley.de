6ffddaf8a3fdb7155ad6613afaa42d605857009b
Michael Holley <michael.philipp.holley@gmail.com>
2026-01-06 12:00:35 +0100

    feat: replaced tailwind postcss with vite version

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { playwright } from '@vitest/browser-playwright';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { defineConfig } from 'vitest/config';
-import { playwright } from '@vitest/browser-playwright';
-import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()],
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
 
 	test: {
 		expect: { requireAssertions: true },

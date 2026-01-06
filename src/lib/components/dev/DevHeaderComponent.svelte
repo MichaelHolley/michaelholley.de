a28b8f1057a53dbba4fe4b1fcff8833fa2aff5ec
Michael Holley <michael.philipp.holley@gmail.com>
2026-01-06 11:38:14 +0100

    feat: minor change to DevHeader

--- src/lib/components/dev/DevHeaderComponent.svelte
+++ src/lib/components/dev/DevHeaderComponent.svelte
@@ -5,14 +5,14 @@
 	const { header, tech }: { header: string; tech?: TechItem[] } = $props();
 </script>
 
-<div class="flex flex-row justify-start gap-6 pb-1">
+<div class="flex flex-row justify-start gap-4 pb-1">
 	<h3 class="text-2xl text-neutral-500">{header}</h3>
 	{#if !!tech}
-		<div class="flex flex-row items-center gap-2">
+		<div class="flex flex-row items-center gap-1">
 			{#each tech as t (t.name)}
 				<Icon
 					icon={t.icon}
-					class={'text-xl opacity-70 saturate-0 transition-all duration-300 hover:rotate-8 hover:opacity-100 hover:saturate-100'}
+					class={'size-5 opacity-70 saturate-0 transition-all duration-300 hover:rotate-8 hover:opacity-100 hover:saturate-100'}
 				/>
 			{/each}
 		</div>

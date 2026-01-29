3372319f17f3287e412dde6cedadd4c3e4be0d75
Michael Holley <michael.philipp.holley@gmail.com>
2026-01-29 22:38:28 +0100

    feat: improve og meta-tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,22 @@
 	import Projects from '$lib/components/sections/ProjectsSection.svelte';
 </script>
 
+<svelte:head>
+	<title>Michael Holley</title>
+	<meta
+		name="description"
+		content="FullStack Developer mit Leidenschaft für innovative Weblösungen. Schau rein!"
+	/>
+	<meta property="og:title" content="Michael Holley" />
+	<meta property="og:type" content="website" />
+	<meta
+		property="og:description"
+		content="FullStack Developer mit Leidenschaft für innovative Weblösungen. Schau rein!"
+	/>
+	<meta property="og:image" content="/MichaelHolleyBanner.webp" />
+	<meta property="og:url" content="https://michaelholley.de" />
+</svelte:head>
+
 <Header />
 <Dev />
 <Experience />

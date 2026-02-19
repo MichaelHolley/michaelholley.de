2b971df4fe0a946d61d57fbd8271fc13401e6cab
Michael Holley <michael.philipp.holley@gmail.com>
2026-02-19 16:53:43 +0100

    feat: vercel conditional adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
-import adapter from '@sveltejs/adapter-node';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterVercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const adapter = process.env.VERCEL ? adapterVercel() : adapterNode();
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,10 +11,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter,
 		experimental: {
 			remoteFunctions: true
 		}

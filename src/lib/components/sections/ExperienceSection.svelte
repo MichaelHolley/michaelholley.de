a55a374ea0e730c0d8e2fcba8e89a1fe6845adc4
Michael Holley <michael.philipp.holley@gmail.com>
2026-06-09 19:09:18 +0300

    fix: remove unused text styling on steps

--- src/lib/components/sections/ExperienceSection.svelte
+++ src/lib/components/sections/ExperienceSection.svelte
@@ -10,22 +10,20 @@
 	<section id="experience" class="bg-white text-black">
 		<div class="container py-8 md:py-16 md:pt-12 2xl:px-64">
 			<SectionHeader title="Experience" class="pb-8 text-center" />
-			<div class="text-left text-lg">
-				{#each experienceSection.Steps as step (step.id)}
-					<Step period={step.Timeframe} title={step.Role} workplace={step.Employer ?? ''}>
-						{#if step.FreeText}
-							<p>{step.FreeText}</p>
-						{/if}
-						{#if step.Work && step.Work.length > 0}
-							<ul class="list-square list-inside *:py-1">
-								{#each step.Work as item (item.id)}
-									<li>{item.WorkEntry}</li>
-								{/each}
-							</ul>
-						{/if}
-					</Step>
-				{/each}
-			</div>
+			{#each experienceSection.Steps as step (step.id)}
+				<Step period={step.Timeframe} title={step.Role} workplace={step.Employer ?? ''}>
+					{#if step.FreeText}
+						<p>{step.FreeText}</p>
+					{/if}
+					{#if step.Work && step.Work.length > 0}
+						<ul class="list-square list-inside *:py-1">
+							{#each step.Work as item (item.id)}
+								<li>{item.WorkEntry}</li>
+							{/each}
+						</ul>
+					{/if}
+				</Step>
+			{/each}
 		</div>
 	</section>
 {/if}
